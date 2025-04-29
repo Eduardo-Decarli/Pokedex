e9041dfaab0a2187e0be838617bd6fd0a5f27c3b
Eduardo-Decarli <decarliedu@gmail.com>
2025-04-29 13:09:48 -0300

    feat: add pokemons in HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,38 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-async function buscarPokemon() {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log('oi ' + data.results[0].name)
+function convertPokemonToHtml(pokemon) {
+
+    const pokemonsOl = document.getElementById('pokemons');
+
+    const pokemonHtml =  `<li class="pokemon">
+        <div class="poke-id">
+            <span class="name">${pokemon.name}</span>
+            <span class="number">#00${pokemon.id}</span>
+        </div>
+        <div class="detail">
+            <ol class="types">
+                <li class="type">Grass</li>
+                <li class="type">Poison</li>
+            </ol>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                alt="${pokemon.name}">
+        </div>
+    </li>`
+
+    pokemonsOl.innerHTML += pokemonHtml
 }
 
-buscarPokemon();
+async function getPokemons(url) {
 
+    const response = await fetch(url);
+    const data = await response.json();
+    const pokemons = data.results;
+    for (let i = 0; i < pokemons.length; i++) {
+        const pokemon = pokemons[i];
+        pokemon.id = i + 1;
+        convertPokemonToHtml(pokemon);
+    }
+}
 
+getPokemons(url)
